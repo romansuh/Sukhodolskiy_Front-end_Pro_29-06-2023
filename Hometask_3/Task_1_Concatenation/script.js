0863c1ce7b8ef7c5ddde01fb4737ee5eae6b924b
romansuh <72558747+romansuh@users.noreply.github.com>
2023-07-10 19:11:43 +0300

    Hometask 3 done

---
+++ Hometask_3/Task_1_Concatenation/script.js
@@ -0,0 +1,5 @@
+var str1 = prompt("First string: ");
+var str2 = prompt("Second string: ");
+var str3 = prompt("Third string: ");
+
+alert(str1 + str2 + str3);
---
+++ Hometask_3/Task_2/script.js
@@ -0,0 +1,4 @@
+var age = prompt("Вкажіть ваш вік: ");
+var name = prompt("Вкажіть ваше ім'я: ");
+
+alert(`Вітаю ${name} \nваш вік ${age}`);
