81205c0fdf0777f4776568f13d86468e2e5c1de6
romansuh <72558747+romansuh@users.noreply.github.com>
2023-07-13 18:55:57 +0300

    Hometask 5 done

---
+++ Hometask_5/script.js
@@ -0,0 +1,6 @@
+var num1 = parseInt(prompt("Enter 1st number: "));
+var num2 = parseInt(prompt("Enter 2nd number: "));
+var num3 = parseInt(prompt("Enter 3rd number: "));
+
+var avr = (num1 + num2 + num3) / 3;
+alert(`Arithmetical mean of ${num1}, ${num2} and ${num3} is ${avr}.`);
