793629315decbd029cf7a41afe607c19c7efbde9
romansuh <72558747+romansuh@users.noreply.github.com>
2023-07-17 17:31:26 +0300

    Hometask 7 done

--- Hometask_5/script.js
+++ Hometask_5/script.js
@@ -1,10 +1,10 @@
-var num1 = prompt("Enter 1st number: ");
-var num2 = prompt("Enter 2nd number: ");
-var num3 = prompt("Enter 3rd number: ");
-
-var avr = (parseFloat(num1) + parseFloat(num2) + parseFloat(num3)) / 3;
-if (avr == Math.round(avr)) {
-    alert(`Arithmetical mean of ${num1}, ${num2} and ${num3} is ${avr}.`);
-} else {
-    alert(`Arithmetical mean of ${num1}, ${num2} and ${num3} is ${avr.toFixed(5)}.`);
-}
+// var num1 = prompt("Enter 1st number: ");
+// var num2 = prompt("Enter 2nd number: ");
+// var num3 = prompt("Enter 3rd number: ");
+//
+// var avr = (parseFloat(num1) + parseFloat(num2) + parseFloat(num3)) / 3;
+// if (avr == Math.round(avr)) {
+//     alert(`Arithmetical mean of ${num1}, ${num2} and ${num3} is ${avr}.`);
+// } else {
+//     alert(`Arithmetical mean of ${num1}, ${num2} and ${num3} is ${avr.toFixed(5)}.`);
+// }
