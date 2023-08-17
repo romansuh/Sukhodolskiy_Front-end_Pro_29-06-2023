9c7025ba6a0097133b493c2881dd57379ca5c4c8
romansuh <72558747+romansuh@users.noreply.github.com>
2023-08-17 15:20:17 +0300

    Hometask 17 done

---
+++ Hometask_17/script.js
@@ -0,0 +1,22 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(this.step);
+        return this;
+    }
+};
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep();
+
+ladder.up().up().down().showStep();
